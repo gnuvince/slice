a2fc09f978f4299adde5f65a4635ccf40438e079
Vincent Foley <vincent.foley-bourgon@lightstep.com>
2023-05-28 07:41:31 -0230

    tests: add system tests

---
+++ tests/system_tests.rs
@@ -0,0 +1,31 @@
+use std::process::Command;
+
+#[test]
+fn two_offsets() {
+    let out = Command::new(env!("CARGO_BIN_EXE_slice"))
+        .arg("tests/test00")
+        .arg("4")
+        .arg("8")
+        .output()
+        .unwrap();
+    assert_eq!(out.stdout, b"efgh");
+}
+
+#[test]
+fn one_offsets() {
+    let out = Command::new(env!("CARGO_BIN_EXE_slice"))
+        .arg("tests/test00")
+        .arg("4")
+        .output()
+        .unwrap();
+    assert_eq!(out.stdout, b"efghijklmnopqrstuvwxyz\n");
+}
+
+#[test]
+fn zero_offsets() {
+    let out = Command::new(env!("CARGO_BIN_EXE_slice"))
+        .arg("tests/test00")
+        .output()
+        .unwrap();
+    assert_eq!(out.stdout, b"abcdefghijklmnopqrstuvwxyz\n");
+}
